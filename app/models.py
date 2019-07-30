5aa0e5fb40b4bc8a1f61c3556e2f7da23e031d82
arsummers <aliyasummers1@gmail.com>
2019-07-29 17:34:51 -0700

    typo fix:

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from app import db
 class Author(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(256), unique=True)
-    books = db.relationship("Books", backref="band", lazy=True)
+    books = db.relationship('Book', backref='author', lazy=True)
 
     def to_dict(self):
         return {'id':self.id, 'name':self.name}
@@ -11,7 +11,8 @@ class Author(db.Model):
 class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(256), unique=True)
-    author_id = db.Column(db.Integer, db.ForeignKey('author_id'), nullabe=True)
+    author_id = db.Column(db.Integer, db.ForeignKey('author.id'), nullable=True)
 
     def to_dict(self):
         return {'id':self.id, 'name':self.name}
+
